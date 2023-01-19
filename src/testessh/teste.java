7e60559ff28da6025a95523f8c65980b310b44a1
luizperezsv <119428791+luizperezsv@users.noreply.github.com>
2023-01-19 16:39:16 -0300

    Versão 1.1.2

--- src/testessh/teste.java
+++ src/testessh/teste.java
@@ -5,7 +5,7 @@ public class teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Teste SSH");
-		
+		System.out.println("Atualizando");
 
 	}
 
