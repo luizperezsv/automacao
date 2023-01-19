ec41facf70c3d42011eca9c6aa04d0bcfb59de0c
luizperezsv <119428791+luizperezsv@users.noreply.github.com>
2023-01-19 16:25:38 -0300

    versão 1.1.1

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author luizperez
+ *
+ */
+module testessh {
+}
---
+++ src/testessh/teste.java
@@ -0,0 +1,12 @@
+package testessh;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste SSH");
+		
+
+	}
+
+}
